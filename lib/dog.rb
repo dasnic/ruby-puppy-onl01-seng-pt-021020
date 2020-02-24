c23d44238f24555a922b651f75925df2eeb5c4ea
Lydell Gaines <dasnic70@gmail.com>
2020-02-24 04:34:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,22 @@
-class Dog  #belong to a shelter & an owner
-  attr_accessor :name, :age, :breed, :shelter, :owner
+class Dog
+
+  attr_accessor :name
+
   @@all = []
-  
-  def initialize(name, age, breed)
+
+  def initialize(name)
     @name = name
-    @age = age
-    @breed = breed 
-    @@all << self 
-  end 
-  
+    @@all << self
+  end
+
   def self.all
-    @@all
-  end 
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
 end
